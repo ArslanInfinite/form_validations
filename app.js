cc52e07a1ca269ae31be25ef00985d9fdfdba1f8
Arslan <ali.arslan319@gmail.com>
2021-10-17 23:46:07 -0400

    create validatePhone with regex and validation to add or remove is-valid class, ensuring multiple different phone number entry formats

--- app.js
+++ app.js
@@ -32,12 +32,19 @@ function validateEmail(){
     const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/
 
     if(!re.test(email.value)){
-        email.classList.add('is-invalid')
+        phone.classList.add('is-invalid')
     } else {
         email.classList.remove('is-invalid')
     }
 }
 
 function validatePhone(){
-    
+    const phone = document.getElementById('phone')
+    const re = /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/
+
+    if(!re.test(phone.value)){
+        phone.classList.add('is-invalid')
+    } else {
+        phone.classList.remove('is-invalid')
+    }
 }
