0ad2943d338a55c5e0f770a2a8bf7157bd2a4b18
Arslan <ali.arslan319@gmail.com>
2021-10-17 23:10:19 -0400

    add event listeners to each form for blur

---
+++ app.js
@@ -0,0 +1,7 @@
+// form Blur eventlisteners 
+
+document.getElementById('name').addEventListener('blur', validateName)
+document.getElementById('zip').addEventListener('blur', validateZip)
+document.getElementById('email').addEventListener('blur', validateEmail)
+document.getElementById('phone').addEventListener('blur', validatePhone)
+
